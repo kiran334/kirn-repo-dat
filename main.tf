64d00c46b05f78a7f4394a21d768a05f7fad0a27
saikiran pinn <kiranpinn9@gmail.com>
2022-08-28 20:37:06 -0400

    testing KV in new repo

---
+++ main.tf
@@ -0,0 +1,14 @@
+# ARM provider block -main
+provider "azurerm" {
+  version = "~>2.0"
+  features {}
+}
+# Terraform backend configuration block -precreated
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform-rg"
+    storage_account_name = "myterraformsadev"
+    container_name       = "tfstatefile"
+    key                  = "keyvault.tfstate"
+  }
+}
