4eaf1dc247fdd6374126b93f92bc74864dee1a83
saikiran pinn <kiranpinn9@gmail.com>
2023-11-19 12:21:02 -0500

    testing-now

--- Az-Storage-Acct.tf
+++ Az-Storage-Acct.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "demo" {
+resource "azurerm_resource_group" "demo1" {
   name     = "dev-rg1119"
   location = "East US2"
 }
@@ -6,8 +6,8 @@ resource "azurerm_resource_group" "demo" {
 ##  Demo now
 resource "azurerm_storage_account" "StorageAccountDemo" {
   name                     = "kirantestsa"
-  resource_group_name      = azurerm_resource_group.demo.name
-  location                 = azurerm_resource_group.demo.location
+  resource_group_name      = azurerm_resource_group.demo1.name
+  location                 = azurerm_resource_group.demo1.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
