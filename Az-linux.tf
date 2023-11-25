eaeb8844a3cdb314415d9ea5628ca8f92f750115
saikiran pinn <kiranpinn9@gmail.com>
2023-11-25 12:06:22 -0500

    testing-with count now

--- Az-linux.tf
+++ Az-linux.tf
@@ -26,7 +26,7 @@ resource "azurerm_network_interface" "example" {
 
 resource "azurerm_linux_virtual_machine" "example" {
   count               =     1
-  name                = "sailinuxnew1125"
+  name                = "kiranlinuxnew1125"
   resource_group_name = var.resource_group_name
   location            = var.location
   size                = "Standard_F2"
