f9f4171d4d9bc35de748def5c101a44f4b0e3e0c
saikiran pinn <kiranpinn9@gmail.com>
2023-12-09 14:31:38 -0500

    testing with folder

---
+++ Linux/main.tf
@@ -0,0 +1,31 @@
+# variable "client_secret" {
+# }
+
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>3.0.0"
+    }
+  }
+  cloud {
+    organization = "spk545"
+    workspaces {
+      name = "Kiran_WS_API_type"
+    }
+  }
+}
+
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name   = "dev-rg"
+#     storage_account_name   = "sadevaccttest"
+#     container_name         = "tffiles"
+#     key                    = "terraform_sql.tfstate"
+#   }
+# }
+provider "azurerm" {
+  features {}
+  
+}
